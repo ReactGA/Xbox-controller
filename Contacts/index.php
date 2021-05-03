ac7468c201c8a4df564dfef95237bec0d2280643
ReactGA <61600583+ReactGA@users.noreply.github.com>
2021-05-03 15:38:38 +0100

    Add files via upload

---
+++ Contacts/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div>
+    
+    <h1>Get in touch</h1>
+    <p>please fill the form</p>
+
+    <form action="contact.php" method = "post">
+    <input type="text" name="name" placeholder="Full Name">
+    <input type="text" name="E-Mail" placeholder="E-Mail">
+    <input type="text" name="subject" placeholder="subject">
+    <textarea name="message" placeholder="Enter Message"></textarea>
+    <Button type="submit" name="submit" >Send Message</Button>
+    </form>
+    
+    </div>
+</body>
+</html>
